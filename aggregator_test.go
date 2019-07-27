b3f0612a35c5ad082e2857a1a1a53d7b1195e42e
Jack Lindamood <cep221@gmail.com>
2019-07-27 00:05:23 -0700

    tests: more agg tests

--- aggregator_test.go
+++ aggregator_test.go
@@ -1,11 +1,13 @@
 package cwmessagebatch
 
 import (
+	"reflect"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_resetToUTC(t *testing.T) {
@@ -42,6 +44,56 @@ func Test_resetToUTC(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := resetToUTC(tt.args.datum)
 			tt.verify(t, got)
+		})
+	}
+}
+
+func Test_clearInvalidUnits(t *testing.T) {
+	type args struct {
+		datum *cloudwatch.MetricDatum
+	}
+	tests := []struct {
+		name string
+		args args
+		want *cloudwatch.MetricDatum
+	}{
+		{
+			name: "good_unit",
+			args: args{
+				datum: &cloudwatch.MetricDatum{
+					Unit: aws.String("Seconds"),
+				},
+			},
+			want: &cloudwatch.MetricDatum{
+				Unit: aws.String("Seconds"),
+			},
+		},
+		{
+			name: "bad_unit",
+			args: args{
+				datum: &cloudwatch.MetricDatum{
+					Unit: aws.String("Second"),
+				},
+			},
+			want: &cloudwatch.MetricDatum{},
+		},
+		{
+			name: "nilunit",
+			args: args{
+				datum: &cloudwatch.MetricDatum{},
+			},
+			want: &cloudwatch.MetricDatum{},
+		},
+		{
+			name: "nildatum",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			if got := clearInvalidUnits(tt.args.datum); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("clearInvalidUnits() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
--- gzipbuilder_test.go
+++ gzipbuilder_test.go
@@ -1,11 +1,12 @@
 package cwmessagebatch
 
 import (
-	"github.com/stretchr/testify/require"
 	"math/rand"
 	"net/http"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/aws/aws-sdk-go/aws/request"
 )
@@ -23,17 +24,14 @@ func reqWithBody(body string) *request.Request {
 }
 
 func Test_buildPostGZip(t *testing.T) {
-	type args struct {
-		r *request.Request
-	}
 	tests := []struct {
-		name string
-		arg  *request.Request
+		name     string
+		arg      *request.Request
 		validate func(r *request.Request)
 	}{
 		{
 			name: "basic",
-			arg: reqWithBody("hello world"),
+			arg:  reqWithBody("hello world"),
 			validate: func(r *request.Request) {
 				require.Equal(t, "gzip", r.HTTPRequest.Header.Get("Content-Encoding"))
 				require.NoError(t, r.Error)
@@ -41,14 +39,14 @@ func Test_buildPostGZip(t *testing.T) {
 		},
 		{
 			name: "should_compress",
-			arg: reqWithBody(strings.Repeat("A", 1024 * 64)),
+			arg:  reqWithBody(strings.Repeat("A", 1024*64)),
 			validate: func(r *request.Request) {
 				require.NoError(t, r.Error)
 			},
 		},
 		{
 			name: "shoulderr",
-			arg: reqWithBody(randomString(1024 * 64)),
+			arg:  reqWithBody(randomString(1024 * 64)),
 			validate: func(r *request.Request) {
 				require.Error(t, r.Error)
 				require.IsType(t, &awsRequestSizeError{}, r.Error)
@@ -56,6 +54,7 @@ func Test_buildPostGZip(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			buildPostGZip(tt.arg)
 			tt.validate(tt.arg)
@@ -65,7 +64,7 @@ func Test_buildPostGZip(t *testing.T) {
 
 func randomString(n int) string {
 	ret := strings.Builder{}
-	for i :=0;i<n;i++ {
+	for i := 0; i < n; i++ {
 		ret.WriteRune(rune(10 + rand.Intn(240)))
 	}
 	return ret.String()
